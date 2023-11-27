2032da97ba2c32dd12198e5e103e91e187fe4c88
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-11-27 03:55:07 -0500

    wip: consumer stuff

---
+++ src/consumer.rs
@@ -0,0 +1,50 @@
+use serde::de::DeserializeOwned;
+
+pub struct Consumer<K, V>
+where
+    K: DeserializeOwned,
+    V: DeserializeOwned,
+{
+    topic: String,
+    _key: std::marker::PhantomData<K>,
+    _value: std::marker::PhantomData<V>,
+}
+
+impl<K, V> Consumer<K, V>
+where
+    K: DeserializeOwned,
+    V: DeserializeOwned,
+{
+    pub fn new(topic: String) -> Consumer<K, V> {
+        Consumer {
+            topic,
+            _key: std::marker::PhantomData,
+            _value: std::marker::PhantomData,
+        }
+    }
+
+    pub fn consume(&mut self) -> Record<K, V>
+    where
+        K: DeserializeOwned + std::fmt::Debug,
+        V: DeserializeOwned + std::fmt::Debug,
+    {
+        println!("Consuming record from topic: {}", self.topic);
+
+        // let mut stream = std::net::TcpStream::connect("localhost:8080").expect("Failed to connect to server");
+        // let mut buffer = [0; 1024];
+        // stream.read(&mut buffer).expect("Failed to read from server");
+
+        // let record: Record<K, V> = serde_json::from_str(std::str::from_utf8(&buffer).expect("Failed to convert buffer to string")).expect("Failed to deserialize record");
+
+        println!("Record consumed: {:?}", record);
+
+        record
+    }
+
+    // pub fn subscribe(&mut self, 
+    //     println!("Subscribing to topic: {}", self.topic);
+    // }
+
+    // pub fn unsubscribe(&mut self) {
+    //     
+}
