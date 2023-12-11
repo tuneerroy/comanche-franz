ce117e813a199cc3263d6900852d983970b26ecd
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-12-11 02:19:02 -0500

    refactor: new structs and types in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+pub type ServerId = u16;
+pub type Topic = String;
+pub type Value = String;
+
 mod broker {
     mod listeners;
     mod requests;
@@ -19,10 +25,51 @@ mod consumer {
 mod producer {
     mod listeners;
     mod requests;
+    mod utils;
 }
 
 mod utils;
 
-pub type PartitionId = String;
-pub type ServerId = u16;
-pub type Topic = String;
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PartitionId {
+    topic: Topic,
+    partition_num: usize,
+}
+
+impl PartitionId {
+    pub fn new(topic: Topic, partition_num: usize) -> PartitionId {
+        PartitionId {
+            topic,
+            partition_num,
+        }
+    }
+}
+
+impl ToString for PartitionId {
+    fn to_string(&self) -> String {
+        format!("{}-{}", self.topic, self.partition_num)
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PartitionInfo {
+    partition_id: PartitionId,
+    server_id: ServerId,
+}
+
+impl PartitionInfo {
+    pub fn new(partition_id: PartitionId, server_id: ServerId) -> PartitionInfo {
+        PartitionInfo {
+            partition_id,
+            server_id,
+        }
+    }
+
+    pub fn partition_id(&self) -> &PartitionId {
+        &self.partition_id
+    }
+
+    pub fn server_id(&self) -> &ServerId {
+        &self.server_id
+    }
+}
