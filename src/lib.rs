2c8675b99b8b71ba1404b8a54b6e6d653b84547f
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-12-11 02:31:31 -0500

    feat: new way of creating a partition info

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,15 @@ impl PartitionInfo {
         }
     }
 
+    pub fn from_str(s: &str) -> PartitionInfo {
+        let mut split = s.split('-');
+        let topic = split.next().unwrap().to_string();
+        let partition_num = split.next().unwrap().parse::<usize>().unwrap();
+        let partition_id = PartitionId::new(topic, partition_num);
+        let server_id = split.next().unwrap().parse::<ServerId>().unwrap();
+        PartitionInfo::new(partition_id, server_id)
+    }
+
     pub fn partition_id(&self) -> &PartitionId {
         &self.partition_id
     }
